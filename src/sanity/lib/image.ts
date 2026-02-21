052f9fa80f74351eb164e7548b5eaab720e74e91
Karla Almeida <almeidakarla.uga@gmail.com>
2026-02-21 14:16:59 -0300

    Fix Sanity image type import

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,11 +1,11 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import type { Image } from 'sanity'
 
 const imageBuilder = createImageUrlBuilder({
   projectId: '9lvs5sql',
   dataset: 'production',
 })
 
-export const urlFor = (source: SanityImageSource) => {
+export const urlFor = (source: Image) => {
   return imageBuilder.image(source)
 }
