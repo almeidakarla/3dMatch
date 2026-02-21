98223cd8d76a1160be24e137c96dcd5f74134bce
Karla Almeida <almeidakarla.uga@gmail.com>
2026-02-21 14:19:50 -0300

    Fix image type to use any for flexibility

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,11 +1,11 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
 
 const imageBuilder = createImageUrlBuilder({
   projectId: '9lvs5sql',
   dataset: 'production',
 })
 
-export const urlFor = (source: Image) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const urlFor = (source: any) => {
   return imageBuilder.image(source)
 }
